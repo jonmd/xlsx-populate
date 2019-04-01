5d47af1f5a394c604966013663c3ec1f609235f4
Jon Moen Drange <jondrange@gmail.com>
2019-04-01 13:38:40 +0200

    Handle DST changes in date <-> number conversion

--- test/unit/dateConverter.spec.js
+++ test/unit/dateConverter.spec.js
@@ -19,6 +19,15 @@ describe("dateConverter", () => {
             expect(dateConverter.dateToNumber(new Date('07 Mar 2015 13:26:24'))).toBe(42070.56);
             expect(dateConverter.dateToNumber(new Date('04 Apr 2017 20:00:00'))).toBeCloseTo(42829.8333333333, 10);
         });
+
+        // TODO This test only works in time zones with DST change on last Sunday in March
+        // and have a base offset of +00:00 to +:10:00.
+        it("should convert dates with correct hour where DST changes occur", () => {
+            // Last Sunday in March 2019: standard time -> DST
+            expect(dateConverter.dateToNumber(new Date('31 Mar 2019 12:00:00'))).toBe(43555.5);
+            // Last Sunday in October 2018: DST -> standard time
+            expect(dateConverter.dateToNumber(new Date('28 Oct 2018 12:00:00'))).toBe(43401.5);
+        });
     });
 
     describe("numberToDate", () => {
@@ -28,6 +37,15 @@ describe("dateConverter", () => {
             expect(dateConverter.numberToDate(61)).toEqual(new Date('01 Mar 1900 00:00:00'));
             expect(dateConverter.numberToDate(42070.56)).toEqual(new Date('07 Mar 2015 13:26:24'));
             expect(dateConverter.numberToDate(42829.8333333333)).toEqual(new Date('04 Apr 2017 20:00:00'));
+        });
+
+        // TODO This test only works in time zones with DST change on last Sunday in March
+        // and have a base offset of +00:00 to +:10:00.
+        it("should convert number to date with correct hour where DST changes occurs", () => {
+            // Last Sunday in March 2019: standard time -> DST
+            expect(dateConverter.numberToDate(43555.5)).toEqual(new Date('31 Mar 2019 12:00:00'));
+            // Last Sunday in October 2018: DST -> standard time
+            expect(dateConverter.numberToDate(43401.5)).toEqual(new Date('28 Oct 2018 12:00:00'));
         });
     });
 });
